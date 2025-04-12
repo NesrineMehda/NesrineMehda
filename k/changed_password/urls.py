35cb9784f7c3f5af2a75078e6522ed306bdff33d
Sirineaya <as.refsi@esi-sba.dz>
2025-04-12 15:53:14 +0100

    serine test password in change passwor urls path

--- k/changed_password/urls.py
+++ k/changed_password/urls.py
@@ -3,7 +3,7 @@ from .views import ForgotPasswordAPIView, ResetPasswordAPIView
 
 urlpatterns = [
     path('forgot_password/', ForgotPasswordAPIView.as_view(), name='forgot_password'),
-    path('reset_password/<uidb64>/<token>/', ResetPasswordAPIView.as_view(), name='reset_password'),
+    path('reset_pasword/<uidb64>/<token>/', ResetPasswordAPIView.as_view(), name='reset_password'),
 ]'''
 
 
